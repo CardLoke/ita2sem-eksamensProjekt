d3974fbcf07efbb81b4c09be6e116e3891bd8f7b
Vitus <vitusfle@gmail.com>
2026-05-04 10:39:13 +0200

    123
    14221

--- WebApp/Core/Model/Users.cs
+++ WebApp/Core/Model/Users.cs
@@ -6,7 +6,7 @@ namespace Core.Model
 {
     public class User
     {
-        public string BrugerNavn { get; set; }
+        public string Username { get; set; }
         public string Mail { get; set; }
 
         public string Password { get; set; }
