164a0fea7396a3ce611f597abf4acac90a59d956
Nikolai Borg <eaa25nbkr@students.eaaa.dk>
2026-05-05 11:27:31 +0200

    Update UserRepositoryMongoDB.cs

--- WebApp/ServerApi/Repositories/UserRepositoryMongoDB.cs
+++ WebApp/ServerApi/Repositories/UserRepositoryMongoDB.cs
@@ -11,8 +11,7 @@ namespace ServerApi.Repositories
 
         public UserRepositoryMongoDB()
         {
-            //var client = new MongoClient("mongodb+srv://kris600m:eyh94zkh@cluster0.xpou06p.mongodb.net/");
-            var client = new MongoClient("mongodb+srv://Semesterprojekt:Hundlika1@cluster0.s0l3m62.mongodb.net/");
+            var client = new MongoClient("mongodb+srv://kris600m:eyh94zkh@cluster0.xpou06p.mongodb.net/");
             var database = client.GetDatabase("SessionSync");
             _Users = database.GetCollection<User>("Users");
 
